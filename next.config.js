97e22dd9813c352d7af06122069450e65df52c4c
basicdev04 <tanishqkandpal.work@gmail.com>
2025-12-27 19:02:59 +0530

    img

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: [
-            "res.cloudinary.com"
-        ]
-    }
-}
+  images: {
+    domains: ["res.cloudinary.com", "www.littlemonsters.fun"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
